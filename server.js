d0a4ae50546f4fdfe110c7a0d7fbb33e05bcdee9
Mario Bruno <mbrunostem@gmail.com>
2021-12-05 16:51:18 -0500

    updated folders

--- server.js
+++ server.js
@@ -4,3 +4,12 @@ const path = require('path');
 
 const PORT = 3001;
 const app = express();
+
+app.get('/api', (req, res) => {
+
+});
+
+app.listen(PORT, () => 
+    console.log('App listening at http://localhost:${PORT}')
+);
+
