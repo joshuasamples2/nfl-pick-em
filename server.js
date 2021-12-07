07beb5807cb93777ee67a37c6887a471b03e557f
Mario Bruno <mbrunostem@gmail.com>
2021-12-06 20:28:50 -0500

    update

--- server.js
+++ server.js
@@ -1,15 +1,40 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+const routes = require('./controllers');
 const path = require('path');
 
-const PORT = 3001;
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.get('/api', (req, res) => {
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
 
-app.listen(PORT, () => 
-    console.log('App listening at http://localhost:${PORT}')
-);
+// app.get('/api', (req, res) => {
 
+// });
+
+// app.listen(PORT, () => 
+//     console.log('App listening at http://localhost:${PORT}')
+// );
