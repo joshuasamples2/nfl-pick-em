bafe26306016d20c486885de1af962f2bb040417
Mario Bruno <mbrunostem@gmail.com>
2021-12-08 19:32:08 -0500

    listening

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,2 +1,8 @@
 const router = require('express').Router();
+const path = require('path');
 
+router.get('/', async (req, res) => {
+    res.render('home');
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,23 +1,3 @@
-// const express = require('express');
-// const exphbs = require('express-handlebars');
-// const path = require('path');
-// const routes = require('./controllers');
-
-
-// const PORT = process.env.PORT || 3001;
-// const app = express();
-// app.use(express.json());
-// app.get('/api', (req, res) => {
-
-// });
-// app.engine('handlebars' , hbs.engine);
-// app.set('view engine', 'handlebars');
-// app.engine('handlebars', exphbs({defaultLayout:"main"}));
-// app.set('view engine', 'handlebars')
-// app.listen(PORT, () => 
-//     console.log(`App listening at http://localhost:${PORT}`)
-// );
-
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
@@ -25,6 +5,8 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -37,7 +19,9 @@ const sess = {
   cookie: {},
   resave: false,
   saveUninitialized: true,
-  
+  store: new SequelizeStore({
+    db: sequelize
+  })
 };
 
 app.use(session(sess));
@@ -52,9 +36,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-
-  app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
-
-// app.listen(PORT, () => 
-//     console.log('App listening at http://localhost:${PORT}')
-// );
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
