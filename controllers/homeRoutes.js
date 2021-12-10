4e37c3759aeaab955469de2ff58a766c6a7a45da
joshuasamples2 <joshua.samples2@gmail.com>
2021-12-09 20:07:01 -0600

    merging

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,5 +4,13 @@ const path = require('path');
 router.get('/', async (req, res) => {
     res.render('home');
 });
-
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect to the homepage
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+    // Otherwise, render the 'login' template
+    res.render('login');
+  });
 module.exports = router;
